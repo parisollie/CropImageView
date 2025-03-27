0c2fc2c6e64b44bf65cd653fd2651b46b1b3dee0
parisollie <pjff18@gmail.com>
2025-03-27 14:23:00 -0600

    Initial Commit

---
+++ CropImageView/CropImageViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CropImageViewApp.swift
+//  CropImageView
+//
+//  Created by Paul F on 27/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CropImageViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
